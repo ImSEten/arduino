58bc4d6da10961fc423db3f618b14f5634a357cc
ImSEten <zhangyu_ok@163.com>
2025-12-13 21:07:20 +0800

    using ld2410b to detech person exist

--- auto-light/auto-light.ino
+++ auto-light/auto-light.ino
@@ -1,31 +1,74 @@
 #include <FastLED.h>
 
-#define LED_PIN   2
+#define PERSON_PIN 2
+#define LED_PIN   3
 #define NUM_LEDS  30
 
-bool start = true;
+bool ledStatus = false;
+int personValue = 0;
 
 CRGB leds[NUM_LEDS];
 
+void set_led_color() {
+  for (int i = 0; i <= NUM_LEDS - 1; i++) { // set color
+    leds[i] = CRGB(255, 150, 40);
+  }
+}
+
+void led_lazy_on() {
+  if (ledStatus) {
+    return;
+  }
+  for (int i = 0; i <= 255; i++) { // light up
+    FastLED.setBrightness(i);
+    FastLED.delay(20);
+    FastLED.show();
+    ledStatus = true;
+  }
+}
+
+void led_lazy_off() {
+  if (!ledStatus) {
+    return;
+  }
+  for (int i = 255; i >= 0; i--) {
+    FastLED.setBrightness(i); // 渐灭
+    FastLED.delay(20);
+    FastLED.show();
+    ledStatus = false;
+  }
+}
+
+bool person_detect() {
+  personValue = digitalRead(PERSON_PIN); // 读取引脚的电平
+  if (personValue == HIGH) {
+  Serial.println("person exist"); // 如果电平为高，打印 "高电平"
+  return true;
+  } else {
+  Serial.println("person not exist"); // 如果电平为低，打印 "低电平"
+  return false;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(PERSON_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT); // LED初始化引脚为输出模式
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  Serial.begin(115200);
+  set_led_color();
+  led_lazy_on();
+  led_lazy_off();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (start) {
-    for (int i = 0; i <= NUM_LEDS - 1; i++) {
-      leds[i] = CRGB(255, 150, 40);
-    }
-    for (int i = 0; i <= 255; i++) {
-      FastLED.setBrightness(i);
-      FastLED.delay(20);
-      FastLED.show();
-    }
-    start = false;
-  } else {
-    FastLED.delay(5000);
+  bool person = person_detect();
+  if (person) { // 有人
+    led_lazy_on();
+    delay(5000); // 等待5s后再检查
+  } else { // 无人
+    led_lazy_off();
+    delay(100);
   }
 }
